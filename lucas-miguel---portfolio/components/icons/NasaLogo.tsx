482cbe3f90da4c5d3b8cfa0f58e581fd52994bb6
folkbarbecue <lucasmiguelbsilva@gmail.com>
2026-01-30 14:39:37 -0300

    Delete lucas-miguel---portfolio directory

--- lucas-miguel---portfolio/components/icons/NasaLogo.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const NasaLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-        <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM8.547 4.505a8.25 8.25 0 0 1 8.355 4.883.75.75 0 0 1-1.32.521A6.75 6.75 0 0 0 9.873 5.828a.75.75 0 0 1-.502-1.323Zm-3.04 4.056a8.25 8.25 0 0 1 10.932-1.76.75.75 0 1 1-.884 1.18A6.75 6.75 0 0 0 6.63 7.852a.75.75 0 0 1-1.124-.971Z" clipRule="evenodd" />
-    </svg>
-);
